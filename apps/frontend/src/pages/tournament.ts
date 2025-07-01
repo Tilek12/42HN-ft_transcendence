acd5ebf0531cdf7354c9360785231af0124c59df
Thomas Nakas <tnakas@2-E-4.42heilbronn.de>
2025-07-01 22:06:50 +0200

    shuffling and round logic for the backend

--- apps/frontend/src/pages/tournament.ts
+++ apps/frontend/src/pages/tournament.ts
@@ -3,19 +3,53 @@ import { renderNav } from './nav'
 type Player = {
 	id: string;
 	name: string;
+	points?: number;
 	winner?: boolean;
   };
   
   type Round = Player[];
-  
+
+  function shuffle<T>(array: T[]) : T[] {
+	const arr = [...array];
+	for (let i = arr.length - 1; i > 0; i--)
+	{
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+	return arr;
+  }
+
   function createRounds(players: Player[]): Round[] {
 	let rounds: Round[] = [];
 	let currentPlayers = players;
   
+	currentPlayers.forEach((player)=>{player.points =  0; player.winner = false;});
 	while (currentPlayers.length > 1) {
-	  rounds.push(currentPlayers);
-	  currentPlayers.forEach((player, i) => {player.winner = i%2 === 0;});
-	  currentPlayers = currentPlayers.filter((player) => player.winner);
+		rounds.push(currentPlayers);
+		currentPlayers.forEach((player)=>{player.points =  0; player.winner = false;});
+		for(let i = 0; i < currentPlayers.length; i+=2)  // a simulation of a game
+		{
+			const player1 = currentPlayers[i];
+			const player2 = currentPlayers[i + 1];
+
+			if(!player2)
+			{
+				player1.winner = true;
+				continue;
+			}
+			while(player1.points < 5 && player2.points < 5)
+			{
+				let pl1 = Math.random();
+				let pl2 = Math.random();
+				if (pl1 > pl2)
+					player1.points++;
+				else
+					player2.points++;
+			}
+			player1.points < 5 ? player2.winner = true : player1.winner = true; 
+		}
+	  currentPlayers = shuffle(currentPlayers.filter((player) => player.winner));
+
 	}
 	rounds.push(currentPlayers);
 	return rounds;
@@ -63,7 +97,8 @@ export function renderTournament(root: HTMLElement) {
 	{ id: 'p9', name: 'Timi' },
 	{ id: 'p10', name: 'Kreg' },
 	{ id: 'p11', name: 'Nick' },
-	{ id: 'p11', name: 'Nele' }
+	{ id: 'p12', name: 'Nele' },
+	{ id: 'p13', name: 'Sol' }
   ];
   
   const root = document.getElementById('app')!;
