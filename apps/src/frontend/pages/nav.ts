0dc153f1abf7b6fb91f01259b5231d63cc37566f
Thomas Nakas <tnakas@2-H-3.42heilbronn.de>
2025-08-27 12:00:12 +0200

    fixing an html on select

--- apps/src/frontend/pages/nav.ts
+++ apps/src/frontend/pages/nav.ts
@@ -97,7 +97,7 @@ export function renderNav() {
             Login
           </a>
 		  <div class="flex items-center space-x-2">
-			<label for="language-select" class="text-white font-semibold">Language:</label>
+			<label for="language-select" class="text-white font-semibold"></label>
 			<select id="language-select" 
 					class="bg-gray-800 text-white px-4 py-2 rounded-lg border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
 				<option value="EN">EN</option>
