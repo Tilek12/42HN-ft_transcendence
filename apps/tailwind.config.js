972cbbeecbdc87d44f265232a4de98d17b56c969
Philipp Strohal <p.strohal@outlook.de>
2025-11-09 16:24:37 +0100

    absolute chaos, dont know what im doing with the setup

---
+++ apps/tailwind.config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    content: ['./src/**/*.{html,ts}'],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
+exports.default = config;
